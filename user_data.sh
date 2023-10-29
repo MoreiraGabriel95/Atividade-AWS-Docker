988ef6c500ee57dec93e6932fa93ec5349bcc0f1
Gabriel Moreira <102887537+MoreiraGabriel95@users.noreply.github.com>
2023-10-29 23:14:52 +0000

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -8,7 +8,7 @@ sudo usermod -aG docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker-compose
-sudo systemctl start docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Faz o deploy de um contêiner de aplicação WordPress
 git clone https://github.com/MoreiraGabriel95/Atividade-AWS-Docker
