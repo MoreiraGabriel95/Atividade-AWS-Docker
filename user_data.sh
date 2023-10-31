219d066cdb78a7bc4245e655353b0830917ab7ce
Gabriel Moreira <102887537+MoreiraGabriel95@users.noreply.github.com>
2023-10-31 03:54:35 +0000

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install git -y
-sudo yum install httpd -y
 sudo amazon-linux-extras install docker -y
 sudo yum install mysql -y
 sudo service docker start
@@ -9,9 +8,42 @@ sudo usermod -aG docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-git clone https://github.com/MoreiraGabriel95/Atividade-AWS-Docker
-cd Atividade-AWS-Docker
-docker-compose -f docker-compose.yml up -d
+sudo mkdir /wordpress-config
+cd /wordpress-config/
+sudo mkdir efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0706de9a2fda442cc.efs.us-east-1.amazonaws.com:/ /wordpress-config/efs
 
-sudo mkdir /efs
-sudo mount -t efs -o tls fsap-09ca9216038ba599e:/ /wordpress-config/efs	
+
+echo "version: '3.1'
+
+services:
+
+  wordpress:
+    image: wordpress
+    restart: always
+    ports:
+      - 80:80
+    environment:
+      WORDPRESS_DB_HOST: db
+      WORDPRESS_DB_USER: exampleuser
+      WORDPRESS_DB_PASSWORD: examplepass
+      WORDPRESS_DB_NAME: exampledb
+    volumes:
+      - wordpress:/var/www/html
+
+  db:
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_DATABASE: exampledb
+      MYSQL_USER: exampleuser
+      MYSQL_PASSWORD: examplepass
+      MYSQL_RANDOM_ROOT_PASSWORD: '1'
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  wordpress:
+  db:" >> docker-compose.yml
+
+docker-compose -f docker-compose.yml up -d
